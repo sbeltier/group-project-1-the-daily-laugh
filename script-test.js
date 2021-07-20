d6d8a6cc59218c9e6316eaa22dbbf7baccb14af6
Xander <xanschwarz@gmail.com>
2021-07-20 15:31:25 -0700

    Each API can be called to display the response using the code within. Will need to be adapted to final HTML.

--- script-test.js
+++ script-test.js
@@ -1,9 +1,15 @@
-var queryURL = "https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,political"
+// Each joke API is coded using fetch and AJAX methods within a function, to use a method with an API just uncomment the appropriate function.
+// Need to look at parameters to filter out NSFW jokes and, in the case of the geek API, chuck norris jokes.
+// Need to test calling a function based on user joke type selection.
 
+// is there a flag for race jokes?
+var randomURL = "https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,political"
 
-fetch (queryURL, {
-    method: 'GET'
-})
+// Using fetch method:
+function randomFetch() {
+    fetch (randomURL, {
+        method: 'GET'
+    })
     .then(function (response){
         return response.json();
     })
@@ -11,9 +17,178 @@ fetch (queryURL, {
         console.log(data);
         var setup = document.getElementById('setup')
         var delivery = document.getElementById('delivery')
-
         setup.innerHTML = data.setup
         delivery.innerHTML = data.delivery
+    })
+}
+// randomFetch();
 
-    }
-)
+// Using AJAX method:
+function randomAjax() {
+    $.ajax({
+        url: randomURL,
+        method: 'GET',})
+    .then(function (response) {
+        var setup = $('#setup');
+        var delivery = $('#delivery');
+        setup.text(response.setup);
+        delivery.text(response.delivery);
+    })
+}
+// randomAjax();
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
+// geek-jokes is full of Chuck Norris jokes also. Way to only show geek jokes?
+var geekURL = "https://geek-jokes.sameerkumar.website/api?format=json";
+
+// Using fetch method:
+function geekFetch() {
+    fetch (geekURL, {
+        method: 'GET'
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then (function (data) {
+        console.log(data);
+        var joke = document.getElementById('joke');
+        joke.innerHTML = data.joke;
+    })
+}
+// geekFetch();
+
+// Using AJAX method:
+function geekAjax() {
+    $.ajax({
+        url: geekURL,
+        method: 'GET',})
+    .then(function (response) {
+        var joke = $('#joke');
+        joke.html(response.joke);
+    })
+}
+// geekAjax();
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
+// Some of these are NSFW. See about parameters
+var chuckNorrisURL = "https://api.chucknorris.io/jokes/random";
+
+// Using fetch method:
+function chuckNorrisFetch() {
+    fetch (chuckNorrisURL, {
+        method: 'GET'
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then (function (data) {
+        console.log(data);
+        var joke = document.getElementById('joke');
+        joke.innerHTML = data.value;
+    })
+}
+// chuckNorrisFetch();
+
+// Using AJAX method:
+function chuckNorrisAjax() {
+    $.ajax({
+        url: chuckNorrisURL,
+        method: 'GET',})
+    .then(function (response) {
+        var joke = $('#joke');
+        joke.html(response.value);
+    });
+}
+// chuckNorrisAjax();
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
+// Some of these are NSFW. See about parameters
+var dadJokeURL = "https://dad-jokes.p.rapidapi.com/random/joke/png";
+var dadJokeKey = "74f08a0113msha07df561725aea0p1786ebjsndfeb58ea0cc2"
+var dadJokeHost = "dad-jokes.p.rapidapi.com"
+
+// Using fetch method:
+function dadJokeFetch() {
+    fetch (dadJokeURL, {
+        method: 'GET',
+        headers: {
+            "x-rapidapi-key": dadJokeKey,
+            "x-rapidapi-host": dadJokeHost,
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then (function (data) {
+        console.log(data);
+        var setup = document.getElementById('setup')
+        var delivery = document.getElementById('delivery')
+        setup.innerHTML = data.body.setup;
+        delivery.innerHTML = data.body.punchline;
+    })
+}
+// dadJokeFetch();
+
+// Using AJAX method:
+function dadJokeAjax() {
+    $.ajax({
+        url: "https://dad-jokes.p.rapidapi.com/random/joke/png",
+        method: 'GET',
+        headers: {
+            "x-rapidapi-key": dadJokeKey,
+		    "x-rapidapi-host": dadJokeHost,
+        }})
+    .then(function (response) {
+        var setup = $('#setup');
+        var delivery = $('#delivery');
+        setup.text(response.body.setup);
+        delivery.text(response.body.punchline);
+    });
+}
+// dadJokeAjax();
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
+var manateeURL = "https://manatee-jokes.p.rapidapi.com/manatees/random/"
+var manateeKey = "74f08a0113msha07df561725aea0p1786ebjsndfeb58ea0cc2"
+var manateeHost = "manatee-jokes.p.rapidapi.com"
+
+// Using fetch method:
+// FETCH METHOD NOT WORKING WITH KEYS
+function manateeFetch() {
+    fetch (manateeURL, {
+        method: 'GET',
+        headers: {
+            "x-rapidapi-key": manateeKey,
+            "x-rapidapi-host": manateeHost,
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then (function (data) {
+        console.log(data);
+        var setup = document.getElementById('setup')
+        var delivery = document.getElementById('delivery')
+        setup.innerHTML = data.setup;
+        delivery.innerHTML = data.punchline;
+    })
+}
+// manateeFetch();
+
+// Using AJAX method:
+function manateeAjax() {
+    $.ajax({
+        url: "https://manatee-jokes.p.rapidapi.com/manatees/random/",
+        method: 'GET',
+        headers: {
+            "x-rapidapi-key": "74f08a0113msha07df561725aea0p1786ebjsndfeb58ea0cc2",
+              "x-rapidapi-host": "manatee-jokes.p.rapidapi.com",
+      }}).then(function (response) {
+        var setup = $('#setup');
+        var delivery = $('#delivery');
+        setup.text(response.setup);
+        delivery.text(response.punchline);
+      });
+}
+// manateeAjax();
