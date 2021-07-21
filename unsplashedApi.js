f7c4819ac8a91fbe8067651a0ad924e1ca0af997
Anthony Zamora <anthony.zamora@gmail.com>
2021-07-20 19:51:08 -0700

    initial

---
+++ unsplashedApi.js
@@ -0,0 +1,14 @@
+$("#myForm").submit(function(event){
+    event.preventDefault()
+
+    var search = $("#search").val()
+    var url = "https://api.unsplash.com/search/photos?query="+search+"&client_id=9pKdm2wau0n_B0J1g4z4yTHfOCJY9lLUft4x21--LD8"
+
+    $.ajax({
+        method:'GET',
+        url:url,
+        success:function(data){
+            console.log(data)
+        }
+    })
+})
