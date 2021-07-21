2aaa534516469c288f3905ffc11f6953600bbfbe
sbeltier <sarahnbeltier@gmail.com>
2021-07-20 19:27:20 -0700

    basic joke2api logic

--- script-test.js
+++ script-test.js
@@ -1,29 +1,51 @@
+// Global Variables
 var queryURL = "https://v2.jokeapi.dev/joke/"
+    // If we want to exclude offensive jokes: https://v2.jokeapi.dev/joke/Any?safe-mode
 var submitCriteria_Form= document.getElementById('criteria-form')
 var jokeCategories = document.querySelectorAll('option')
 
+// First Button - What type of joke are you in the mood for?
 submitCriteria_Form.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log(event.target.jokeCategories.innerHTML);
+
+   // Condition: only if Joke2API is selected
+   if ($('#joke-category option:selected')){
+
+    // Grab user input from options and add to queryURL
+    var selected_option_jokeCategory = $('#joke-category option:selected')[0].innerHTML
+    console.log("Joke category selected is:")
+    console.log(selected_option_jokeCategory)
+    queryURL = queryURL + selected_option_jokeCategory + "?safe-mode"
 
 
-    // check category and add to query URL
+    var selected_input_blacklist = $('#joke2api-blacklist input:checked')
+    for (i=0; i < selected_input_blacklist.length; i++){
+        console.log("selected_input_blacklist is:")
+        console.log(selected_input_blacklist[i].value)
+    }
+
+    // Get Joke
+    getJoke2Api(queryURL);
 }
-)
 
-    // fetch (queryURL, {
-    //     method: 'GET'
-    // })
-    //     .then(function (response){
-    //         return response.json();
-    //     })
-    //     .then (function (data){
-    //         console.log(data);
-    //         var setup = document.getElementById('setup')
-    //         var delivery = document.getElementById('delivery')
-    
-    //         setup.innerHTML = data.setup
-    //         delivery.innerHTML = data.delivery
-    
-    //     }
-    // )
+})
+
+// Get a joke from Joke2API
+function getJoke2Api (){
+    $.ajax ({
+        url: queryURL,
+        method: 'GET'
+    })
+    .then (function (response){
+        console.log(response);
+        var setup = $('#setup');
+        console.log(setup)
+        var delivery = $('#delivery')
+        console.log(response.setup)
+        console.log(typeof response.setup)
+
+        // Add joke to page
+        setup.html(response.setup)
+        delivery.html(response.delivery)
+    })
+}
